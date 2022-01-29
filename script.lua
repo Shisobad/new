71f6cf4967fcf7a2ec0a3632872f2da5eaccc4c5
Shisobad <87376373+Shisobad@users.noreply.github.com>
2022-01-29 20:12:07 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,22 @@
+local id = game.PlaceId
+if id == 3102144307 then --ACS
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/ACS/main/script.lua")()
+elseif id == 6299805723 then --AFS
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/AFS/main/script.lua")()
+elseif id == 8357510970 then --APS
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/APS/main/script.lua")()  
+elseif id == 7114796110 then --ATS
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/ATS/main/script.lua")() 
+elseif id == 2753915549 or id == 4442272183 or id == 7449423635 then --BF
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/BF/main/script.lua")()  
+elseif id == 4520749081 or id == 6381829480 then --KL
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/KL/main/script.lua")()   
+elseif id == 6284583030 then --PetX
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/PetX/main/script.lua")() 
+elseif id == 7056922815 then --Reaper
 
+elseif id == 7485253727 then --TruePiece
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/TruePiece/main/script.lua")()  
+elseif id == 5303541547 then --TwoPiece
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DookDekDEE/TwoP/main/script.lua")()
+end
